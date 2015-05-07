fd9a79e36237b95c0b5608e1f82681e66b2543f1
Kamil Jonaszko <kamil.jonaszko@wp.pl>
2015-05-07 12:16:15 +0200

    pole

---
+++ src/kamajo5/Tools/Kalkulator.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace kamajo5\Tools;
+
+class Kalkulator
+{
+    private $a;
+    private $h;
+    public function setA($a)
+    {
+        $this->a = $a;
+        return $this;
+    }
+    public function setH($h)
+    {
+        $this->h = $h;
+        return $this;
+    }
+    public function getA()
+    {
+        return $this->a;
+    }
+    public function getH()
+    {
+        return $this->h;
+    }
+
+    public function triangle()
+    {
+        return $this->a * $this->h * 0.5;
+    }
+}
